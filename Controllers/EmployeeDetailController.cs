d3e2e3abd46690af5785e125438293131ce920e4
tahahasan88 <60012801+tahahasan88@users.noreply.github.com>
2020-09-16 00:29:58 +0400

    employee css created for popup

--- Controllers/EmployeeDetailController.cs
+++ Controllers/EmployeeDetailController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TaskManager.Web.Models;
 
 namespace TaskManager.Web.Controllers
 {
@@ -10,7 +11,14 @@ namespace TaskManager.Web.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            TaskCreateViewModel createVM = new TaskCreateViewModel();
+            return PartialView("Index", createVM);
+        }
+
+        public IActionResult Details()
+        {
+            TaskCreateViewModel createVM = new TaskCreateViewModel();
+            return View(createVM);
         }
     }
 }
