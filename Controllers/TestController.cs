7ec0f806792230f5a062753210307e997e81a587
tahahasan88 <60012801+tahahasan88@users.noreply.github.com>
2020-10-18 15:41:08 +0400

    identity username test code

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            ViewData["IdentityUser"] = Environment.UserName;
+            return View();
+        }
+    }
+}
